ba578167eb9f2f879b899e2788d54edbf6fd2e83
Timibreez <ffftimi@gmail.com>
2022-01-11 14:27:02 +0100

    main commit

--- designs.js
+++ designs.js
@@ -2,31 +2,29 @@
 const gridColor = document.querySelector('#colorPicker');
 // Select size input
 const size = document.querySelector('#sizePicker');
+// declare global variables
 const grid = document.querySelector('#pixelCanvas');
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWidth');
 
-size.addEventListener('submit', function(e){
-    e.preventDefault();
-    const height = document.querySelector('#inputHeight').value;
-    const width = document.querySelector('#inputWidth').value;
-    makeGrid(height, width);
-});
-
-
+size.addEventListener('submit', makeGrid);
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid(x, y) {
+function makeGrid(e) {
 
 // Your code goes here!
+    e.preventDefault();
     grid.innerHTML = '';
-    for (let i = 1; i <= x; i++){
+    for (let i = 1; i <= height.value; i++){
         const tr = document.createElement('tr');
         tr.id = 'table' + i;
-        grid.appendChild(tr);
-        for (let j = 1; j <= y; j++){
+        
+        for (let j = 1; j <= width.value; j++){
             const td = document.createElement('td');
             tr.appendChild(td);
         }
+        grid.appendChild(tr);
     }
 
     // add color to each tr
